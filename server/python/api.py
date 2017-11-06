302bdd767b21f1f5e23af37b649487b6a77d4681
Jay Su <jsu317@gmail.com>
2017-11-06 23:15:29 +0800

    fix sensor / status update

--- server/python/api.py
+++ server/python/api.py
@@ -1,69 +1,204 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import MySQLdb
 import paho.mqtt.publish as publish
 
+db_user = 'iot'
+db_pass = 'Kw/hy7D8JB*^4?IX+daDC+NJV$n5C$ofSBZQ4rOB|hyUgzDBMyIx4&O1YE0e2j7x'
+db_name = 'iot'
+db=MySQLdb.connect(user=db_user, passwd=db_pass, db=db_name)
+db.autocommit(True)
 
 app = Flask(__name__)
 api = Api(app)
 
-MQTT = "server.iot.pwnass.com"
+MQTT = "127.0.0.1"
 PREFIX = "XYCS"
 
-class Machine(Resource):
+class CommandMachine(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "machine"),
                 request.form["data"], hostname=MQTT)
         return {"data": request.form["data"]}
 
-class OpMode(Resource):
+class CommandOpMode(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "opmode"),
                 request.form["data"], hostname=MQTT)
         return {"data": request.form["data"]}
 
-class Fan(Resource):
+class CommandFan(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "fan"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class EP(Resource):
+class CommandEP(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "EP"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class UV(Resource):
+class CommandUV(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "UV"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class Ozone(Resource):
+class CommandOzone(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "ozone"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class Ion(Resource):
+class CommandIon(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "ion"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class Buzzer(Resource):
+class CommandBuzzer(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "buzzer"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-class PwmDuty(Resource):
+class CommandPwmDuty(Resource):
     def post(self, sn):
         publish.single("{}/{}/command/{}".format(PREFIX, sn, "pwm_duty"),
                 request.form["data"], hostname=MQTT)
+        return {"data": request.form["data"]}
 
-api.add_resource(Machine, '/XYCS/<string:sn>/command/machine')
-api.add_resource(OpMode, '/XYCS/<string:sn>/command/opmode')
-api.add_resource(Fan, '/XYCS/<string:sn>/command/fan')
-api.add_resource(EP, '/XYCS/<string:sn>/command/EP')
-api.add_resource(UV, '/XYCS/<string:sn>/command/UV')
-api.add_resource(Ozone, '/XYCS/<string:sn>/command/ozone')
-api.add_resource(Ion, '/XYCS/<string:sn>/command/ion')
-api.add_resource(Buzzer, '/XYCS/<string:sn>/command/buzzer')
-api.add_resource(PwmDuty, '/XYCS/<string:sn>/command/pwm_duty')
+api.add_resource(CommandMachine, '/XYCS/<string:sn>/command/machine')
+api.add_resource(CommandOpMode, '/XYCS/<string:sn>/command/opmode')
+api.add_resource(CommandFan, '/XYCS/<string:sn>/command/fan')
+api.add_resource(CommandEP, '/XYCS/<string:sn>/command/EP')
+api.add_resource(CommandUV, '/XYCS/<string:sn>/command/UV')
+api.add_resource(CommandOzone, '/XYCS/<string:sn>/command/ozone')
+api.add_resource(CommandIon, '/XYCS/<string:sn>/command/ion')
+api.add_resource(CommandBuzzer, '/XYCS/<string:sn>/command/buzzer')
+api.add_resource(CommandPwmDuty, '/XYCS/<string:sn>/command/pwm_duty')
+
+class StatusMachine(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "machine"))
+        return {"data": c.fetchone()[0]}
+
+class StatusOpMode(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "opmode"))
+        return {"data": c.fetchone()[0]}
+
+class StatusFan(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "fan"))
+        return {"data": c.fetchone()[0]}
+
+class StatusEP(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "EP"))
+        return {"data": c.fetchone()[0]}
+
+class StatusUV(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "UV"))
+        return {"data": c.fetchone()[0]}
+
+class StatusOzone(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "ozone"))
+        return {"data": c.fetchone()[0]}
+
+class StatusIon(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "ion"))
+        return {"data": c.fetchone()[0]}
+
+class StatusBuzzer(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "buzzer"))
+        return {"data": c.fetchone()[0]}
+
+class StatusPwmDuty(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from status_value where client_id = %s and status_id = (select id from status where name = %s);""", (sn, "pwm_duty"))
+        return {"data": c.fetchone()[0]}
+
+api.add_resource(StatusMachine, '/XYCS/<string:sn>/status/machine')
+api.add_resource(StatusOpMode, '/XYCS/<string:sn>/status/opmode')
+api.add_resource(StatusFan, '/XYCS/<string:sn>/status/fan')
+api.add_resource(StatusEP, '/XYCS/<string:sn>/status/EP')
+api.add_resource(StatusUV, '/XYCS/<string:sn>/status/UV')
+api.add_resource(StatusOzone, '/XYCS/<string:sn>/status/ozone')
+api.add_resource(StatusIon, '/XYCS/<string:sn>/status/ion')
+api.add_resource(StatusBuzzer, '/XYCS/<string:sn>/status/buzzer')
+api.add_resource(StatusPwmDuty, '/XYCS/<string:sn>/status/pwm_duty')
+
+class SensorTemperature(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "temperature"))
+        return {"data": c.fetchone()[0]}
+
+class SensorHumidity(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "humidity"))
+        return {"data": c.fetchone()[0]}
+
+class SensorFanCurrent(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "fan_current"))
+        return {"data": c.fetchone()[0]}
+
+class SensorPrdCurrent(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "prd_current"))
+        return {"data": c.fetchone()[0]}
+
+class SensorUV(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "UV"))
+        return {"data": c.fetchone()[0]}
+
+class SensorOzone(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "ozone"))
+        return {"data": c.fetchone()[0]}
+
+class SensorPM25(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "PM25"))
+        return {"data": c.fetchone()[0]}
+
+class SensorMotion(Resource):
+    def get(self, sn):
+        c = db.cursor()
+        c.execute("""select value from sensor_value where client_id = %s and sensor_id = (select id from sensor where name = %s);""", (sn, "motion"))
+        return {"data": c.fetchone()[0]}
+
+api.add_resource(SensorTemperature, '/XYCS/<string:sn>/sensor/temperature')
+api.add_resource(SensorHumidity, '/XYCS/<string:sn>/sensor/humidity')
+api.add_resource(SensorFanCurrent, '/XYCS/<string:sn>/sensor/fan_current')
+api.add_resource(SensorPrdCurrent, '/XYCS/<string:sn>/sensor/prd_current')
+api.add_resource(SensorUV, '/XYCS/<string:sn>/sensor/UV')
+api.add_resource(SensorOzone, '/XYCS/<string:sn>/sensor/ozone')
+api.add_resource(SensorPM25, '/XYCS/<string:sn>/sensor/PM25')
+api.add_resource(SensorMotion, '/XYCS/<string:sn>/sensor/motion')
+
 if __name__ == '__main__':
     app.run(debug=True);
--- server/python/gatherer.py
+++ server/python/gatherer.py
@@ -5,28 +5,30 @@ db_user = 'iot'
 db_pass = 'Kw/hy7D8JB*^4?IX+daDC+NJV$n5C$ofSBZQ4rOB|hyUgzDBMyIx4&O1YE0e2j7x'
 db_name = 'iot'
 db=MySQLdb.connect(user=db_user, passwd=db_pass, db=db_name)
+db.autocommit(True)
 
 def on_message(client, userdata, msg):
+    '''
     print("message topic=", msg.topic)
     print("message received=" , str(msg.payload.decode('UTF-8')))
+    '''
     _, client_id, msg_type, msg_name = msg.topic.split('/')
     c = db.cursor()
     if msg_type == "sensor":
         c.execute(
-            """insert into sensor_value (client_id, sensor_id, value) select %s, id, %s from sensor where name = %s;""",
-            (client_id, str(msg.payload.decode('UTF-8')), msg_name))
+            """update sensor_value set value = %s where client_id = %s and sensor_id = (select id from sensor where name = %s);""",
+            (str(msg.payload.decode('UTF-8')), client_id, msg_name))
     elif msg_type == "status":
         c.execute(
-            """insert into status_value (client_id, status_id, value) select %s, id, %s from status where name = %s;""",
-            (client_id, str(msg.payload.decode('UTF-8')), msg_name))
-    db.commit()
+            """update status_value set value = %s where client_id = %s and status_id = (select id from status where name = %s);""",
+            (str(msg.payload.decode('UTF-8')), client_id, msg_name))
 
 if __name__ == '__main__':
-    client_name = "PyClient"
-    mqtt_host = "iot.eclipse.org"
+    client_name = "Gatherer"
+    mqtt_host = "127.0.0.1"
     mqtt_port = 1883
     client = mqtt.Client(client_name)
     client.on_message = on_message
     client.connect(mqtt_host, mqtt_port)
-    client.subscribe("XYCS/01/#")
+    client.subscribe("XYCS/#")
     client.loop_forever()
